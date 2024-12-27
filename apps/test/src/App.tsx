ee90f6ff49d707a81896f99aba3af8ba25f71fc0
chanooda <hanrhfqkq@gmail.com>
2024-12-27 17:55:55 +0900

    chore: 패키지 배포 준비

--- apps/test/src/App.tsx
+++ apps/test/src/App.tsx
@@ -1,18 +1,6 @@
-import type { ChildrenProps } from '@chanoooo/libs';
-import { useQuery } from '@chanoooo/libs/react';
+import { ChildrenProps } from '@chanooda/libs-react';
 
 function App({ children }: ChildrenProps) {
-  const { data } = useQuery({
-    queryKey: [''],
-    queryFn: async () => {
-      return await fetch('https://api.example.com').then((res) => {
-        return res.json();
-      });
-    },
-  });
-
-  console.log(data);
-
   return <div>{children}</div>;
 }
 
